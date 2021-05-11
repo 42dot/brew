9bcb93b26e58f616f57334e73655130eaf7badbd
정성환 <scryner@42dot.ai>
2021-05-12 01:07:03 +0900

    initial commit

---
+++ Formula/vaulter.rb
@@ -0,0 +1,28 @@
+class Vaulter < Formula
+  desc "A swiss-knife CLI for 42dot infra"
+  homepage "https://gitlab.42dot.ai/vaulter.git"
+  url "git://gitlab.42dot.ai:sysadmin/vaulter.git"
+  version ""
+  sha256 ""
+  license "42dot proprietary"
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test vaulter`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
