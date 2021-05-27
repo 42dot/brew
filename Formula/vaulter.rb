9b193c86c322b98c634b23c9a8f987274c0d521f
정성환 <scryner@42dot.ai>
2021-05-27 10:15:14 +0900

    updating vaulter v1.0.0-beta.4

--- Formula/vaulter.rb
+++ Formula/vaulter.rb
@@ -2,18 +2,18 @@ class Vaulter < Formula
   desc "A CLI tool for 42dot"
   homepage "https://gitlab.42dot.ai/vaulter.git"
   url "https://gitlab.42dot.ai/sysadmin/vaulter.git",
-       tag:      "v1.0.0-beta.3",
-       revision: "b1d0c87f5e763637007c63dbe67273dc2ef3cf4c"
+       tag:      "v1.0.0-beta.4",
+       revision: "06baa984967b565dbc910429ddc1ad9453d9a8ad"
 
-  version "v1.0.0-beta.3"
+  version "v1.0.0-beta.4"
   license "42dot proprietary"
 
-  depends_on "go" => :build
-
   bottle do
-    root_url "https://binplate.42dot.systems/brew/vaulter/v1.0.0-beta.3"
-    sha256 cellar: :any_skip_relocation, big_sur: "d1df91cd38ab2b1a91732f49cc2d98483a599ddc0f626088711701c946f15841"
+    root_url "https://binplate.42dot.systems/brew/vaulter/v1.0.0-beta.4"
+    sha256 cellar: :any_skip_relocation, big_sur: "49de7d31a973a87055c0c9ba719a4f9069e56005e2a095fff5238e41a9cfe901"
   end
+
+  depends_on "go" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
