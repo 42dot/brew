a5237edc29eb39409bc7f7d35ea8642acd9b91fb
정성환 <scryner@42dot.ai>
2021-07-26 17:50:16 +0900

    updating to version 1.0.0-beta.5

--- Formula/vaulter.rb
+++ Formula/vaulter.rb
@@ -2,15 +2,15 @@ class Vaulter < Formula
   desc "A CLI tool for 42dot"
   homepage "https://gitlab.42dot.ai/vaulter.git"
   url "https://gitlab.42dot.ai/sysadmin/vaulter.git",
-       tag:      "v1.0.0-beta.4",
-       revision: "06baa984967b565dbc910429ddc1ad9453d9a8ad"
+       tag:      "v1.0.0-beta.5",
+       revision: "6dcc550ef9f7eaf94df8be3d120afb73f9b6ec9c"
 
-  version "v1.0.0-beta.4"
+  version "v1.0.0-beta.5"
   license "42dot proprietary"
 
   bottle do
-    root_url "https://binplate.42dot.systems/brew/vaulter/v1.0.0-beta.4"
-    sha256 cellar: :any_skip_relocation, big_sur: "49de7d31a973a87055c0c9ba719a4f9069e56005e2a095fff5238e41a9cfe901"
+    root_url "https://binplate.42dot.systems/brew/vaulter/v1.0.0-beta.5"
+    sha256 cellar: :any_skip_relocation, big_sur: "88ce4e425bb65eedcb11045065c0460b2435b73eebad91711d65ed936a8bae48"
   end
 
   depends_on "go" => :build
